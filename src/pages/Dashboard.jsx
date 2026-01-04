a1ceac475a5c7775991664bcadc457cf6fbfa1a8
amirali72 <amirali531361@gmail.com>
2026-01-04 20:37:07 +0530

    removed unwanted code from dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => {
-  const { logout } = useAuth();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
   return (
     <div>
-      <Navbar/>
-      <h1>Dashboard</h1>
-      <button onClick={handleLogout}>Logout</button>
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">Dashboard</div>
+      </div>
     </div>
   );
 };
