7e74948b7de683828ec4176bb8be88e96deef5c2
amirali72 <amirali531361@gmail.com>
2026-01-05 01:07:37 +0530

    added link to logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { PiFediverseLogoBold } from "react-icons/pi";
 import { FaUser } from "react-icons/fa";    
 import { useAuth } from "../context/AuthContext";
 import { IoLogOut } from "react-icons/io5";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -15,10 +15,10 @@ const Navbar = () => {
     <>
       <nav className="w-full shadow-lg transition duration-300 flex h-12">
         <div className="m-2 flex justify-between w-full">
-          <div className="flex">
+          <Link to={"/dashboard"} className="flex">
             <PiFediverseLogoBold className="self-center text-blue-800 text-xl" />
             <h2 className="self-center font-semibold px-2">Incident Ops</h2>
-          </div>
+          </Link>
           <div className="flex">
             <FaUser className="self-center text-blue-800 text-lg"/>
             <h1 className="self-center px-2 text-sm">{user.name}</h1>
