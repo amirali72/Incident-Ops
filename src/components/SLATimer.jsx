19026f4de87a6774de6a10e7d9a6b1844df2a944
amirali72 <amirali531361@gmail.com>
2026-01-16 00:07:46 +0530

    Modifications in timer

--- src/components/SLATimer.jsx
+++ src/components/SLATimer.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { FaCircle } from "react-icons/fa";
 
-const SLATimer = ({item, deadline,hours, minutes, remaining}) => {
-  return item.status === "Closed" || item.status === "Resolved" ? (
+const SLATimer = ({ openedAt, slaHours, status }) => {
+  const deadline = openedAt + slaHours * 60 * 60 * 1000;
+  const remaining = deadline - Date.now();
+  const hours = Math.floor(Math.abs(remaining) / (1000 * 60 * 60));
+  const minutes = Math.floor(
+    (Math.abs(remaining) % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  return status === "Closed" || status === "Resolved" ? (
     <span className="text-green-600 flex space-x-1.5">
       <FaCircle className="self-center" />
       <h1>Completed</h1>
